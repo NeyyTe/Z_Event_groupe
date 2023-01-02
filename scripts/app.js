95aa583798a63f3db9da5847ee14d4e9646d3a61
NeyyTe <marechal.maxxence@oulook.fr>
2023-01-02 23:22:03 +0100

    Page index et asso lié

--- scripts/app.js
+++ scripts/app.js
@@ -12,62 +12,6 @@ if(document.querySelector('.tab-content')) {
                contentTab[i].classList.add('active')
             }
         })
-    } 
+    }} 
 
-    //  buttonTab[0].addEventListener('click', () => {
-    //     if(!contentTab[0].classList.contains('active')) {
-    //         contentTab.forEach(element => {
-    //         element.classList.remove('active');
-    //         })
-    //         contentTab[0].classList.add('active')
-    //         }
-    //     })
-    //     buttonTab[1].addEventListener('click', () => {
-    //         if(!contentTab[1].classList.contains('active')) {
-    //             contentTab.forEach(element => {
-    //             element.classList.remove('active');
-    //             })
-    //             contentTab[1].classList.add('active')
-    //             }
-    //         })
-    //         buttonTab[2].addEventListener('click', () => {
-    //             if(!contentTab[2].classList.contains('active')) {
-    //                 contentTab.forEach(element => {
-    //                 element.classList.remove('active');
-    //                 })
-    //                 contentTab[2].classList.add('active')
-    //                 }
-    //             })
-    //             buttonTab[3].addEventListener('click', () => {
-    //                 if(!contentTab[3].classList.contains('active')) {
-    //                     contentTab.forEach(element => {
-    //                     element.classList.remove('active');
-    //                     })
-    //                     contentTab[3].classList.add('active')
-    //                     }
-    //                 })
-    //                 buttonTab[4].addEventListener('click', () => {
-    //                     if(!contentTab[4].classList.contains('active')) {
-    //                         contentTab.forEach(element => {
-    //                         element.classList.remove('active');
-    //                         })
-    //                         contentTab[4].classList.add('active')
-    //                         }
-    //                     })
-    //                     buttonTab[5].addEventListener('click', () => {
-    //                         if(!contentTab[5].classList.contains('active')) {
-    //                             contentTab.forEach(element => {
-    //                             element.classList.remove('active');
-    //                             })
-    //                             contentTab[5].classList.add('active')
-    //                             }
-    //                         })
-    //                         buttonTab[6].addEventListener('click', () => {
-    //                             if(!contentTab[6].classList.contains('active')) {
-    //                                 contentTab.forEach(element => {
-    //                                 element.classList.remove('active');
-    //                                 })
-    //                                 contentTab[6].classList.add('active')
-    //                                 }
-    //                             })
-}
+    
